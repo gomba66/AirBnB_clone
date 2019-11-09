4e6e37362792dae41109cca86bee5a8c4f69e299
Gomba66 <danielalejo66@hotmail.com>
2019-11-09 21:45:59 +0000

    [Feature] Serialization and Deserialization[task 4]

--- models/base_model.py
+++ models/base_model.py
@@ -8,8 +8,14 @@ from datetime import datetime
 class BaseModel:
     """ BaseModel Class is the base model"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Method that inicialize the class """
+        if kwargs:
+            for keys in kwargs:
+                if keys == 'name':
+                    self.name = kwargs[keys]
+                if keys == 'my_number':
+                    self.my_number = kwargs[keys]
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
