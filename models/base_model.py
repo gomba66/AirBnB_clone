4ef00d68188b63350b547bb8fe585cf4b3a8015c
Gomba66 <danielalejo66@hotmail.com>
2019-11-09 21:22:17 +0000

    [Feature] I added the new item class to the dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -15,14 +15,15 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
 
-
     def to_dict(self):
-        self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        return self.__dict__
+        self.created_at = str(self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f"))
+        self.updated_at = str(self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f"))
+        dic = self.__dict__.copy()
+        dic["__class__"] = self.__class__.__name__
+        return dic
