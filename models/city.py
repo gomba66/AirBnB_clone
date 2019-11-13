2e505b0e47f2ad33cf10f686541d00f804a7f571
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-11-13 18:42:33 -0500

    city

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""City Class"""
+
+
+import models
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """class that inherits from BaseModel"""
+
+    state_id = ""
+    name = ""
