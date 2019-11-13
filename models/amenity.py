57f4b822d4c4f8017180c01f59315a2281830399
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-11-13 18:43:07 -0500

    amenity

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Amenety Class"""
+
+
+import models
+from models.base_model import BaseModel
+
+
+class Amenety:
+    """class that inherits from BaseModel"""
+
+    name = ""
