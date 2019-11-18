f42abb3039acb78d9613161afe50dccd48a8efaf
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-11-17 19:15:22 -0500

    storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -22,3 +22,23 @@ class TestBaseModelDocs(unittest.TestCase):
             os.remove("file.json")
         except BaseException:
             pass
+
+def test_saves_new_instance(self):
+        """Tests if file is being created """
+        b1 = BaseModel()
+        models.storage.new(b1)
+        models.storage.save()
+        file_exist = os.path.exists(self.file_path)
+        self.assertTrue(file_exist)
+
+def test_new(self):
+        """Tests the new method"""
+        temp_storage = FileStorage()
+        temp_dict = temp_storage.all()
+        Holberton = User()
+        Holberton.id = 972
+        Holberton.name = "Holberton"
+        temp_storage.new(Holberton)
+        class_name = Holberton.__class__.__name__
+        key = "{}.{}".format(class_name, str(Holberton.id))
+        self.assertIsNotNone(temp_dict[key])
